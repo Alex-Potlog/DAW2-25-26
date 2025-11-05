03c66bfd61fcac95499ee4c47fe1d0916116ac64
Alex-Potlog <alexpotloghary@gmail.com>
2025-11-05 17:04:14 +0100

    Created utils folder

--- utils/asignExamen.sh
+++ utils/asignExamen.sh
@@ -1,6 +1,6 @@
 nom=$1
-mkdir "Examen-${nom}"
-cd "Examen-${nom}"
+mkdir "../Examen-${nom}"
+cd "../Examen-${nom}"
 mkdir apuntes
 mkdir ejercicios
 find . -mindepth 1 -maxdepth 2 -type d -exec touch {}/archivo.txt \;
