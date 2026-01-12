fd95cb4178916feb136a70005669a19b397221e2
Alex-Potlog <alexpotloghary@gmail.com>
2026-01-12 15:34:57 +0100

    Commit petitó

---
+++ Alex/M6/RA4/exercicisPrimers.js
@@ -0,0 +1,116 @@
+const usuaris = [
+    {
+        nom: "Joan", 
+        edat: 45, 
+        professio: "mecànic", 
+        sou: 1750, 
+        idioma: ["espanyol"]
+    },
+    {
+        nom: "Pere", 
+        edat: 57, 
+        professio: "administratiu", 
+        sou: 1860, 
+        idioma: ["espanyol", "catala", "francès"]
+    },
+    {
+        nom: "Laia", 
+        edat: 24, 
+        professio: "imformatica", 
+        sou: 1500, 
+        idioma: ["espanyol", "catala", "anglès", "francès"]
+    },
+    {
+        nom: "Joana", 
+        edat: 88, 
+        professio: "jubilada", 
+        sou: 480, 
+        idioma: ["catala"]
+    },
+    {
+        nom: "Mark", 
+        edat: 71, 
+        professio: "jubilat", 
+        sou: 650, 
+        idioma: ["anglès"]
+    },
+    {
+        nom: "Josep", 
+        edat: 21, 
+        professio: "estudiant", 
+        sou: 0, 
+        idioma: ["espanyol", "catala", "anglès"]
+    },
+    {
+        nom: "Maria", 
+        edat: 19, 
+        professio: "estudiant", 
+        sou: 0, 
+        idioma: ["espanyol", "catala", "anglès", "francès"]
+    },
+    {
+        nom: "Eva", 
+        edat: 24, 
+        professio: "periodista", 
+        sou: 2750, 
+        idioma: ["espanyol", "catala", "italià", "francès"]
+    },
+    {
+        nom: "Mireia", 
+        edat: 36, 
+        professio: "perruquera", 
+        sou: 1240, 
+        idioma: ["espanyol", "catala"]
+    },
+    {
+        nom: "Esteve", 
+        edat: 54, 
+        professio: "dentista", 
+        sou: 4507, 
+        idioma: ["espanyol", "francès"]
+    },
+    {
+        nom: "Joaquim", 
+        edat: 62, 
+        professio: "jubilat", 
+        sou: 1100, 
+        idioma: ["espanyol", "catala"]
+    },
+    {
+        nom: "Ernest", 
+        edat: 14, 
+        professio: "estudiant", 
+        sou: 0, 
+        idioma: ["catala", "anglès"]
+    },
+    {
+        nom: "Eric", 
+        edat: 28, 
+        professio: "disenyador", 
+        sou: 850, 
+        idioma: ["espanyol", "catala", "anglès", "alemany"]
+    },
+    {
+        nom: "Maiol", 
+        edat: 20, 
+        professio: "estudiant", 
+        sou: 0, 
+        idioma: ["espanyol", "catala"]
+    },
+    {
+        nom: "Carles", 
+        edat: 18, 
+        professio: "estudiant", 
+        sou: 0, 
+        idioma: ["espanyol"]
+    },
+    {
+        nom: "Antoni", 
+        edat: 32, 
+        professio: "metge", 
+        sou: 7800, 
+        idioma: ["espanyol", "catala", "anglès"]
+    }
+];
+
+usuaris.map((user.sou) => user.sou * 1.1);
